62d00a9120b71ad2d4c18c5678813fabd3262c13
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-08-03 19:34:37 -0400

    Assembly and plug-in information

---
+++ Stiletto.Core/Properties/StilettoInfo.cs
@@ -0,0 +1,48 @@
+using BepInEx;
+
+using System.Reflection;
+
+
+#region Assembly information
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stiletto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stiletto")]
+[assembly: AssemblyCopyright("Copyright ©  2020-2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Stiletto.Stiletto.Version)]
+[assembly: AssemblyFileVersion(Stiletto.Stiletto.Version)]
+#endregion
+
+namespace Stiletto
+{
+    public partial class Stiletto : BaseUnityPlugin
+    {
+        public const string GUID = "com.essu.stiletto";
+        public const string Version = "2.3.1";
+        public const int WindowId = 670;
+#if DEBUG
+        public const string PlugInName = "Stiletto (Debug)";
+#else
+        public const string PlugInName = "Stiletto";
+#endif
+
+    }
+}
--- Stiletto/Properties/AssemblyInfo.cs
+++ Stiletto/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Stiletto")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stiletto")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("48db985d-7f30-470e-9c49-af3f3c0c6aa3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion(Stiletto.Stiletto.Version)]
